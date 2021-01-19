db9c6819ade92a3e0c75cd14232ea034b5265d0e
pravin47 <bagul.pravin2@gmail.com>
2021-01-19 21:50:40 +0530

    headtailwin

--- coinsimulation.sh
+++ coinsimulation.sh
@@ -1,9 +1,17 @@
 #!/bin/bash 
 head=1
+hea=0
+tai=0
+for((i=0;i<50;i++))
+do
 random=$((RANDOM%2))
-if(( $random == $head ))
-then
-	echo "head"
-else
-	echo "tail"
-fi
+
+	if(( $random == $head ))
+	then
+	        (( hea++ ))
+	else
+		(( tai++ ))
+	fi
+done
+echo "head wins $hea  time"
+echo "tail wins $tai time"
